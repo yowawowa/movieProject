ccb46da6d86dcb35e593bee6923d700076d439ce
Vladislav Kostin <92290777+yowawowa@users.noreply.github.com>
2023-09-28 16:53:51 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import Movie from './components/Movie'
 import { useState, useEffect } from 'react'
 
 function App() {
-  const API_KEY = 'AT6RB72-1ZAMEV4-HQEEMGP-S4DWRPB'
+  const API_KEY = 'AT6RB72-1ZAMEV4-HQEEMGP-S4DWRPB' // not safe
   const API_URL_RANDOM = 'https://api.kinopoisk.dev/v1.3/movie/random'
   const API_URL_TOP = 'https://api.kinopoisk.dev/v1.3/movie?page=1&limit=10&top10=%21null'
-  const API_URL_SEARCH = 'https://api.kinopoisk.dev/v1.2/movie/search?page=1&limit=10&query=inception'
+  const API_URL_SEARCH = 'https://api.kinopoisk.dev/v1.2/movie/search?page=1&limit=5&query='
 
 
   const [movie, setMovie] = useState([])
+  const [term, setTerm] = useState('')
 
   useEffect(() => {
-    fetch(API_URL_TOP, {
+    MovieFetch(API_URL_TOP)
+  }, [])
+
+
+  const onHandleSearch = (e) => {
+    setTerm(e.target.value)
+  }
+
+  const MovieFetch = (API) => {
+    fetch(API, {
       headers: {
         "Content-Type": "application/json",
         "X-API-KEY": API_KEY,
@@ -20,14 +30,15 @@ function App() {
     })
       .then(res => res.json())
       .then(res => setMovie(res.docs))
-  }, [])
+
+  }
 
   return (
     <>
       <header className='header'>
         <div className='header__content'>
           <a href='/' className='header__logo'>Cinema</a>
-          <input className='header__search' type='text' placeholder='Search' />
+          <input className='header__search' type='text' placeholder='Search' value={term} onChange={onHandleSearch} />
         </div>
       </header>
 
